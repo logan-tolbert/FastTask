03adafaa3a2b3b5d3cc76808f97add0fc29ff765
Logan <lodev205@gmail.com>
2025-07-30 06:55:24 -0500

    🦺 Add guard clauses for TaskItem
    - Introduce `Ardalis.GuardClauses` for input validation in `TaskItem`.
    - Change `UpdatedDate` to nullable `DateTime?`.
    - Add a constructor to initialize properties with guard clauses.

--- src/FastTask.TaskItems/Entities/TaskItem.cs
+++ src/FastTask.TaskItems/Entities/TaskItem.cs
@@ -1,5 +1,5 @@
 ﻿using FastTask.TaskItems.Enums;
-
+using Ardalis.GuardClauses;
 namespace FastTask.TaskItems.Entities;
 
 internal class TaskItem
@@ -9,7 +9,15 @@ internal class TaskItem
     public string Description { get; set; } = string.Empty;
     public ItemStatus Status { get; set; } = ItemStatus.Pending;
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    public DateTime UpdatedDate { get; set;}
+    public DateTime? UpdatedDate { get; set;}
     
+    internal TaskItem(Guid itemId, string title, string description, ItemStatus status,  DateTime createdDate, DateTime updatedDate)
+    {
+        ItemId = Guard.Against.Default(itemId);
+        Title = Guard.Against.NullOrWhiteSpace(title);
+        Description = Guard.Against.NullOrWhiteSpace(description);
+        Status = status;
+        CreatedDate = createdDate;
+    }
 }
 
